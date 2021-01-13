90b173653c73f1740b98338849589ff6c83aabe6
Shivam Bhattacharjee <shivam.bhattacharjee94@gmail.com>
2021-01-13 15:38:52 +0530

    init widget test

---
+++ src/components/Toggle.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Toggle = props => {
+	return (
+		<>
+			<div className="toggle-container">
+				<button className="toggle-btn">Buy Trees</button>
+			</div>
+		</>
+	);
+};
+
+export default Toggle;
